604c05f24951c579c5b26ac3e815a6630b64bb91
Hugo Dias <hugomrdias@gmail.com>
2023-07-18 17:42:54 +0100

    chore: remove wasm stuff

--- examples/wasm/src/workerd.js
+++
@@ -1,6 +0,0 @@
-// This file is inserted into ./web by the build script
-
-import WASM from '../dist/web/wasm_bg.wasm'
-import { initSync } from '../dist/web/wasm.js'
-initSync(WASM)
-export * from '../dist/web/wasm.js'
